a1023ef8d4713e51c57dcddb241dd7d20d455ad5
Vladyslav Korzun <korzun.v.a@gmail.com>
2018-04-13 22:58:09 +0300

    Created project

---
+++ MKELab/MKELab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MKELab
+//
+//  Created by Vladyslav Korzun on 13.04.2018.
+//  Copyright © 2018 Vladyslav Korzun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
